da1d0e21a9199cd00363a18feb7a207f712bf775
Sabbagh <anthonysabbagh03@gmail.com>
2021-11-27 17:55:03 +0100

    feat: add vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import { createApp } from 'vue'
 import { createHead } from '@vueuse/head'
+import {createI18n} from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
 import { store } from './store'
+import { messages, defaultLocale } from "@/translations";
 import './assets/index.postcss'
 
 const head = createHead()
 const app = createApp(App)
 
-app.use(router)
-app.use(store)
+const i18n = createI18n({
+    messages,
+    locale: defaultLocale,
+    fallbackLocale: defaultLocale
+});
 
-app.use(head)
+app.use(router)
+    .use(store)
+    .use(head)
+    .use(i18n)
+
 
 app.mount('#app')
