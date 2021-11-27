582b8df900ebffb6fd8e8edf21efcadd438a54c7
Sabbagh <anthonysabbagh03@gmail.com>
2021-11-27 17:53:01 +0100

    update : reoder package import

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { createApp } from 'vue'
+import { createHead } from '@vueuse/head'
 import App from './App.vue'
 import router from './router'
-import { createHead } from '@vueuse/head'
 import { store } from './store'
 import './assets/index.postcss'
 
 const head = createHead()
 const app = createApp(App)
 
-app.use(store)
 app.use(router)
+app.use(store)
+
 app.use(head)
 
 app.mount('#app')
